f0e50acdf949a6fbb3fbc302c298791d7aab6514
asifjalaldev <asifjalal24796@gmail.com>
2024-08-06 04:42:05 -0500

    create view for register user

--- account/views.py
+++ account/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from account import serializer as account_serializers
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import Group
 # Create your views here.
-def login(request):
-    pass
+
+class RegisterUser(APIView):
+    serializer_class = account_serializers.MyUserSerializer
+
+    def post(self, request):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
