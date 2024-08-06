d3eb10d9b021bb5388c42b59e4b4f9dc84095383
asifjalaldev <asifjalal24796@gmail.com>
2024-08-06 04:40:03 -0500

    add role field in MyUser model to implement role base authorization

--- account/models.py
+++ account/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+ROLE_CHOICES=(
+    ('Buyer', 'Buyer'),
+    ('Seller', 'Seller')
+)
 class MyUser(AbstractUser): 
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.CharField(max_length=255, blank=True, null=True)
@@ -9,4 +13,4 @@ class MyUser(AbstractUser):
     country = models.CharField(max_length=100, blank=True, null=True)
     postal_code = models.CharField(max_length=20, blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    
+    role=models.CharField(max_length=150, choices=ROLE_CHOICES, blank=True, null=True)
