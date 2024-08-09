2f28bae7add1d36b8734e5057faec9047b41531b
asifjalaldev <asifjalal24796@gmail.com>
2024-08-09 17:12:02 +0500

    implement jwt authentication

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from account.views import RegisterUser, LoginUserView
-
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUserView.as_view(), name='login'),
+    path('token/refresh/',TokenRefreshView.as_view(), name='refresh_token'),
+    path('token/', TokenObtainPairView.as_view(), name='access_token')
 
 ]
--- ecomProj/settings.py
+++ ecomProj/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "rest_framework_simplejwt",
     'account',
 ]
 
@@ -125,3 +126,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL='account.MyUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
