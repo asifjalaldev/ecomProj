15f04acce4b4368144b78f2349df3702bf2826a7
asifjalaldev <asifjalal24796@gmail.com>
2024-08-13 12:46:11 +0500

    create product models

---
+++ products/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+
+class ProductImage(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(
+                upload_to='product_images',
+                null=True, blank=True
+                )
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class ProductBrand(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class CategoryAttributes(models.Model):
+    category = models.ForeignKey(
+            ProductCategory,
+            on_delete=models.CASCADE, 
+            null=True, blank=True
+            )
+    
+    attribute_name = models.CharField(max_length=150)
+    attribute_value = models.CharField(max_length=150)
+
+
+class Product(models.Model):
+    product_images = models.ForeignKey(
+        ProductImage, 
+        on_delete=models.CASCADE, 
+        null=True, blank=True
+        )
+
+    product_category = models.ForeignKey(
+        ProductCategory,
+        on_delete=models.CASCADE,
+        null=True, blank=True
+        )
+    
+    product_brand = models.ForeignKey(
+        ProductBrand, 
+        on_delete=models.CASCADE, 
+        null=True, blank=True
+        )
+    
+    name = models.CharField(max_length=150)
+    price = models.DecimalField(decimal_places=2)
+    description = models.TextField()
+    
